bfa8c586acd77a9ba3eb5e8a2582e00e1fc1f155
vanes carrer <vanes.carrer@musixmatch.com>
2023-09-21 13:38:41 +0000

    feat(calculator): add exp op

--- index.js
+++ index.js
@@ -1,22 +1,22 @@
-
-
 //node index.js / 3 4
-const main = () =>{
-    const supportedOperation = ['add', 'sub', 'div', 'mul']
+const main = () => {
 
     const [ , , operation, first, second] = process.argv
 
-    if(!supportedOperation.includes(operation)){
-        throw new Error('Operation not supported')
-    }
+    console.log(`Welcome to the workshop, you provided the following params: ops:${operation}, ${first}, ${second} `)
 
     const calculator = {
-        add: (first, second) => first + second,
+        add: (first, second) => Number(first) + Number(second),
         sub: (first, second) => first - second,
         mul: (first, second) => first * second,
-        div: (first, second) => first / second
+        div: (first, second) => first / second,
+        exp: (first, second) => Math.pow(first,second),
     }
+
+    if(!calculator[operation])  throw new Error('Operation not supported')
 
     console.log('Output: ', calculator[operation](first, second))
 
 }
+
+main()
